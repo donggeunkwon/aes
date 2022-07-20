505235408c5b88b7ff12bffc99b728e4992c8e2b
Donggeun Kwon <43208906+donggeunkwon@users.noreply.github.com>
2022-07-20 14:36:07 +0900

    ModuleNotFoundError is fixed

---
+++ aes/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Donggeun Kwon (donggeun.kwon at gmail.com)
+
+Cryptographic Algorithm Lab.
+Institute of Cyber Security & Privacy(ICSP), Korea Univ.
+"""
+
+# class
+from .aes import *
